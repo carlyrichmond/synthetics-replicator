10ad2e5ecaf210d3c7791d2c1e9470899c3355ee
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-01-10 15:12:17 +0000

    Renaming journey file

---
+++ apps/synthetics-replicator-tests/journeys/orders.journey.ts
@@ -0,0 +1,18 @@
+import { journey, step, monitor, expect } from '@elastic/synthetics';
+
+journey('Replicator Order Journey', ({ page, params }) => {
+  // Only relevant for the push command to create
+  // monitors in Kibana
+  monitor.use({
+    id: 'synthetics-replicator-monitor',
+    schedule: 10,
+  });
+  step('launch application', async () => {
+    await page.goto(params.url);
+  });
+
+  step('assert home page loads', async () => {
+    const header = await page.locator('h1');
+    expect(await header.textContent()).toBe('Replicatr');
+  });
+});
