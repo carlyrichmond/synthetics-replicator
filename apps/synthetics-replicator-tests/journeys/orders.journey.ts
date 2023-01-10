f82e036beb6645263a84eae95987b3e74a383e18
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-01-10 15:31:23 +0000

    Adding move to order page step

--- apps/synthetics-replicator-tests/journeys/orders.journey.ts
+++ apps/synthetics-replicator-tests/journeys/orders.journey.ts
@@ -15,4 +15,15 @@ journey('Replicator Order Journey', ({ page, params }) => {
     const header = await page.locator('h1');
     expect(await header.textContent()).toBe('Replicatr');
   });
+
+  step('assert move to order page', async () => {
+    const orderButton = await page.getByTestId('order-button');
+    await orderButton.click();
+    
+    const url = page.url();
+    expect(url).toContain('/order');
+
+    const menuTiles = await page.getByTestId('menu-item-card');
+    expect(await menuTiles.count()).toBeGreaterThan(2);
+  });
 });
