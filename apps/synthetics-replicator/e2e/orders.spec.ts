f8781b17d3d5918ae3e9f17ac12bd0d0699b3f2d
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-10-10 15:14:09 +0100

    Adding playwright test

---
+++ apps/synthetics-replicator/e2e/orders.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test('assert home page loads', async ({ page }) => {
+  await page.goto('/');
+
+  const header = await page.locator('h1');
+  expect(await header.textContent()).toBe('Replicatr');
+});
+
+test('assert move to order page', async ({ page }) => {
+  await page.goto('/');
+
+  const orderButton = await page.getByTestId('order-button');
+  await orderButton.click();
+  
+  const url = page.url();
+  expect(url).toContain('/order');
+
+  const menuTiles = await page.getByTestId('menu-item-card');
+  expect(await menuTiles.count()).toBeGreaterThan(2);
+});
+
+test('assert adding to order', async ({ page }) => {
+  await page.goto('/order');
+
+  const addItemButtons = await page.getByTestId('add-item-button');
+  expect(await addItemButtons.count()).toBeGreaterThan(10);
+
+  const cartCount = await page.getByTestId('cart-count-label');
+  expect(await cartCount.innerText()).toBe('0');
+
+  await addItemButtons.first().click();
+  expect(await cartCount.innerText()).toBe('1');
+
+  await addItemButtons.nth(4).click();
+  await addItemButtons.last().click();
+  expect(await cartCount.innerText()).toBe('3');
+});
