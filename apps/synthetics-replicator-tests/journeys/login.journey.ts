cd0bad28701f97dea889315a33e2f267de1791b4
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-01-16 16:44:32 +0000

    Formatting

--- apps/synthetics-replicator-tests/journeys/login.journey.ts
+++ apps/synthetics-replicator-tests/journeys/login.journey.ts
@@ -8,7 +8,7 @@ journey('Replicator Login Journey', ({ page, params }) => {
     schedule: 10,
   });
 
-  before(async ()=> {
+  before(async () => {
     await page.goto(params.url);
   });
 
@@ -21,54 +21,54 @@ journey('Replicator Login Journey', ({ page, params }) => {
     const loginButton = await page.getByTestId('account-button');
     expect(loginButton).toHaveText('Login');
     await loginButton.click();
-    
+
     const url = page.url();
     expect(url).toContain('/login');
 
-    const loginForm  = await page.getByTestId('login-form');
+    const loginForm = await page.getByTestId('login-form');
     expect(loginForm).toBeDefined();
   });
 
   step('assert manual login successful', async () => {
-    const loginForm  = await page.getByTestId('login-form');
-  expect(loginForm).toBeDefined();
+    const loginForm = await page.getByTestId('login-form');
+    expect(loginForm).toBeDefined();
 
-   // Check submit button is disabled before entry
-   const submitButton = await page.getByTestId('login-button');
-   expect(submitButton).toBeDisabled();
+    // Check submit button is disabled before entry
+    const submitButton = await page.getByTestId('login-button');
+    expect(submitButton).toBeDisabled();
 
-   // Add credentials
-   const username = params.username;
-   const usernameInput = page.getByTestId('username-input');
-   usernameInput.fill(username)
-   await expect(usernameInput).toHaveValue(username);
+    // Add credentials
+    const username = params.username;
+    const usernameInput = page.getByTestId('username-input');
+    usernameInput.fill(username);
+    await expect(usernameInput).toHaveValue(username);
 
-   const password = params.password;
-   const passwordInput = page.getByTestId('password-input');
-   passwordInput.fill(password)
-   await expect(passwordInput).toHaveValue(password);
+    const password = params.password;
+    const passwordInput = page.getByTestId('password-input');
+    passwordInput.fill(password);
+    await expect(passwordInput).toHaveValue(password);
 
-   // Check login button is now enabled
-   expect(submitButton).toBeEnabled();
-   await submitButton.click();
+    // Check login button is now enabled
+    expect(submitButton).toBeEnabled();
+    await submitButton.click();
 
-   // Wait for login
-   await page.waitForURL('**/order');
+    // Wait for login
+    await page.waitForURL('**/order');
 
-   // Validate we have logged in successfully 
-   const loginButton = await page.getByTestId('account-button');
-   expect(loginButton).toContainText('Welcome');
-   
-   // Ensure we have navigated to menu
-   const menuTiles = await page.getByTestId('menu-item-card');
-   expect(await menuTiles.count()).toBeGreaterThan(0);
+    // Validate we have logged in successfully
+    const loginButton = await page.getByTestId('account-button');
+    expect(loginButton).toContainText('Welcome');
+
+    // Ensure we have navigated to menu
+    const menuTiles = await page.getByTestId('menu-item-card');
+    expect(await menuTiles.count()).toBeGreaterThan(0);
   });
 
   step('assert unable to go to login page when signed in', async () => {
     const loginButton = await page.getByTestId('account-button');
     expect(loginButton).toContainText('Welcome');
     await loginButton.click();
-    
+
     const url = page.url();
     expect(url).toContain('/order');
   });
